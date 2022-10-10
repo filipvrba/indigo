ec1303417a928723a35ac6885c79d1e5cfd806a2
Filip Vrba <filipvrba@pm.me>
2022-10-10 16:20:55 +0200

    fix not_found page

--- lib/indigo/src/app.rb
+++ lib/indigo/src/app.rb
@@ -1,3 +1,12 @@
 require "./lib/via/lib/index"
 require_relative "arguments"
 
+cont_name = @options[:g_controller].downcase
+files = {
+  controller: "src/controllers/#{cont_name}.rb",
+  config: "config.ru",
+  db: "src/db.rb",
+  json: "share/#{cont_name}.json",
+  view: "views/#{cont_name}"
+}
+
--- src/controllers/root.rb
+++ src/controllers/root.rb
@@ -6,7 +6,9 @@ module Controllers
       @title = "#{@db.root.parse(:app_name)} | Root"
       @meta[:og_site_name] = @title
 
-      ren(:root)
+      ren(:root) do
+        erb @via.render "root/index"
+      end
     end
   end
 end
